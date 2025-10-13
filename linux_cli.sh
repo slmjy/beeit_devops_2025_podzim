04a8dc5b51335e284d4014e7b250c55fa725e8e5
ttom <tereza.tomaskova0@gmail.com>
2025-10-13 18:03:40 +0200

    Task 5

--- linux_cli.sh
+++ linux_cli.sh
@@ -18,3 +18,21 @@ fi
 echo ""
 echo "=== Environmentální proměnné ==="
 printenv
+
+
+# ukol 5
+#logovaci funkce
+log() { echo "[INFO] $1"; }
+logError() { echo "[ERROR] $1" >&2; }
+
+#vytvareni linku
+create_link() { [[ $3 == "soft" ]] && ln -s "$1" "$2" || ln "$1" "$2"; }
+
+#vypis aktualizaci
+list_updates() { git update-git-for-windows; echo $?; }
+
+#provedeni aktualizaci
+update_packages() { git update-git-for-windows}
+
+# === Hledání souborů s 'b','e','a','e' v pořadí ===
+najdi_pismena() { find "${1:-.}" -type f -name "*b*e*e*" 2>/dev/null; }
